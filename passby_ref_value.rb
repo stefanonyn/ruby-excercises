63573ac342b906500a25801b4ccdd0f299116794
Stefano Schiavi <stefano@bvprojects.org>
2013-10-30 00:22:10 -0700

    some more exercises

--- passby_ref_value.rb
+++ passby_ref_value.rb
@@ -3,8 +3,26 @@ a = [1,2,3,4,5]
 def change_arr a
 
 	a.join
-	puts a
 	
 end
 
 change_arr a
+puts a 
+
+
+def change_arr1 a
+
+	a.unshift(0)
+	
+end
+
+change_arr1 a 
+puts a
+
+outer_var = "hi I am an outer variable"
+
+2.times do 
+  outer_var.chop
+end
+
+puts outer_var
