90ce24cdaa687e3fef8224db6c31a1a520d32e7d
Stefano Schiavi <stefano@bvprojects.org>
2013-11-01 12:06:25 -0700

    fix bug

--- count_worlds.rb
+++ count_worlds.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 # These are just to make the map easier for me to read.
 # "M" is visually more dense than "o".
 M = 'land'
@@ -18,11 +18,12 @@ world = [[o,o,o,o,o,o,o,o,o,o,o],
 
 def continent_size world, x, y
 
- # if x.between?(0,(world.length-1)) || y.between?(0,(world[x].length-1))
- # puts world[x][y]
 
-binding.pry
- if  world[y][x]==nil || world[y][x] != 'land'
+if x.between?(0,(world.length-1)) and y.between?(0,(world[0].length-1))
+
+
+# binding.pry
+ if world[y][x] != 'land'
 # Either it's water or we already counted it,
 # but either way, we don't want to count it now.
 return 0
@@ -44,6 +45,9 @@ size = size + continent_size(world, x-1, y+1)
 size = size + continent_size(world, x , y+1)
 size = size + continent_size(world, x+1, y+1)
 size
-# end
+else
+  return 0
+end
+
 end
 puts continent_size(world, 0, 10)
