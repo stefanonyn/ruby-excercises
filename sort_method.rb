996831472b7c88970e8e6afeed26d79838e5e079
Stefano Schiavi <stefano@bvprojects.org>
2013-11-01 19:59:35 -0700

    custom sort method

---
+++ sort_method.rb
@@ -0,0 +1,34 @@
+arr = [ "aaa", "bravo", "beach", "bell", "zebra", "mother", "father", "tiger", "love", "aaa", "love"]
+
+
+def my_sort unsorted
+  arr_size = unsorted.length
+  
+  sorted = []
+
+  while sorted.length < arr_size
+    test_element = unsorted.pop
+    not_minor = 0
+
+    unsorted.each do |char|
+      if test_element <= char
+        true
+      else
+        not_minor += 1
+      end   
+    end
+    
+    if not_minor == 0
+
+      sorted.push(test_element)
+
+    else
+
+    unsorted.unshift(test_element)
+    
+    end
+  end
+  sorted
+end
+
+puts my_sort(arr)
